d292dbab939cd0c857f84b6a4663c8b3e38733f2
bghalami <benjaminghalami@gmail.com>
2018-12-04 16:50:17 -0700

    Setup DB, created seed file, and created api/v1/favorites endpoint

---
+++ db/seeds/dev/seed_songs_playlists.js
@@ -0,0 +1,33 @@
+var pry = require('pryjs')
+
+exports.seed = function (knex, Promise) {
+  return knex('playlist_songs').del()
+    .then(() => knex('playlists').del())
+    .then(() => knex('songs').del())
+    .then(() => {
+      return Promise.all([
+        knex('songs').insert({ name: 'Bicycle', artist_name: 'Queen', genre: 'Rock', song_rating: 85, favorite: false }, 'id')
+          .then(song1 => {
+            return knex('songs').insert({ name: 'We Will Rock You', artist_name: 'Queen', genre: 'Rock', song_rating: 95, favorite: true }, 'id')
+            .then(song2 => {
+              return knex('playlists').insert({name: 'Rock Songs'}, 'id')
+              .then(playlist1 => {
+                return knex('playlists').insert({name: 'Swan Songs'}, 'id')
+                .then(playlist2 => {
+                  return knex('playlist_songs').insert({song_id: song1[0], playlist_id: playlist1[0]})
+                  .then(() => {
+                    return knex('playlist_songs').insert({song_id: song1[0], playlist_id: playlist2[0]})
+                    .then(() => {
+                      return knex('playlist_songs').insert({song_id: song2[0], playlist_id: playlist2[0]})
+                    })
+                  })
+                })
+              })
+            })
+          })
+          .then(() => console.log('Seeding complete!'))
+          .catch(error => console.log(`Error seeding data: ${error}`))
+      ])
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
---
+++ server.js
@@ -0,0 +1,26 @@
+const express    = require('express');
+const app        = express();
+const bodyParser = require('body-parser');
+
+const environment   = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database      = require('knex')(configuration);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Play-play';
+
+app.get('/api/v1/favorites', (request, response) => {
+  database('songs').select(['id', 'name', 'artist_name', 'genre', 'song_rating'])
+    .then((songs) => {
+      response.status(200).json(songs);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
