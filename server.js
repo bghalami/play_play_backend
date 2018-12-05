abe6eb0f82f6f02eb9c062606c9aab13bb65863e
bghalami <benjaminghalami@gmail.com>
2018-12-04 17:40:27 -0700

    Created get song by id api, and created post song id

--- server.js
+++ server.js
@@ -21,6 +21,42 @@ app.get('/api/v1/favorites', (request, response) => {
     });
 });
 
+app.get('/api/v1/songs/:id', (request, response) => {
+  database('songs').select(['id', 'name', 'artist_name', 'genre', 'song_rating'])
+  .where("id", request.params.id)
+    .then((songs) => {
+      response.status(200).json(songs);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+app.post('/api/v1/songs', (request, response) => {
+  const song = request.body
+
+  const requiredParameters = ['name', 'artist_name', 'genre', 'song_rating']
+  for(let parameter of requiredParameters) {
+    if (!song[parameter]) {
+      return response
+        .status(400)
+        .send({error: `Missing "${parameter}"`})
+    } else if (song['song_rating'] > 100 || song['song_rating'] < 1) {
+      return response
+        .status(400)
+        .send({error: 'Song Rating must be between 1-100'})
+    }
+  }
+
+  database('songs').insert(song, ['id', 'name', 'artist_name', 'genre', 'song_rating'])
+    .then((song) => {
+      response.status(200).json({"songs": song[0]});
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
