87ece306407552a089787f6b721d628a0975162e
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-12-10 11:27:49 -0700

    Pulled out Song.create into Song model.

--- server.js
+++ server.js
@@ -23,30 +23,7 @@ app.get('/api/v1/favorites', Song.all);
 
 app.get('/api/v1/songs/:id', Song.find);
 
-app.post('/api/v1/songs', (request, response) => {
-  const song = request.body
-
-  const requiredParameters = ['name', 'artist_name', 'genre', 'song_rating']
-  for(let parameter of requiredParameters) {
-    if (!song[parameter]) {
-      return response
-        .status(400)
-        .send({error: `Missing "${parameter}"`})
-    } else if (song['song_rating'] > 100 || song['song_rating'] < 1) {
-      return response
-        .status(400)
-        .send({error: 'Song Rating must be between 1-100'})
-    }
-  }
-
-  Song.create(song)
-    .then((song) => {
-      response.status(200).json({"songs": song[0]});
-    })
-    .catch((error) => {
-      response.status(500).json({ error });
-    });
-});
+app.post('/api/v1/songs', Song.create);
 
 app.patch('/api/v1/songs/:id', function (request, response) {
   const song   = request.body;
