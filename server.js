f423bccc41b8ced1900a03afa56e6dfb1bcca2bd
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-12-11 13:51:11 -0700

    Deletes unused lines in server.js

--- server.js
+++ server.js
@@ -6,15 +6,9 @@ const SearchController = require('./controllers/api/v1/search_controller')
 const LandingController = require('./controllers/api/v1/landing_controller')
 const ArtistController = require('./controllers/api/v1/artist_controller')
 const TracksController = require('./controllers/api/v1/artist/tracks_controller')
-const Song       = require('./lib/models/song');
-const Playlist   = require('./lib/models/playlist');
 const express    = require('express');
 const app        = express();
 const bodyParser = require('body-parser');
-
-const environment   = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
-const database      = require('knex')(configuration);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
